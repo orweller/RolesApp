4160c172eb0062fc27e7acc907059e2bc2c88ed4
Dave Orweller <david.orweller@tithe.ly>
2025-03-07 12:44:15 -0500

    Add update user profile modal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,23 @@ class UsersController < ApplicationController
   end
 
   def update_profile
+    @user = Current.user
+    if @user.update(user_params)
+      redirect_to edit_user_profile_path, notice: "Profile updated."
+    else
+      render :edit
+    end
   end
 
   def manage_password
   end
 
   def update_password
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email_address)
   end
 end
